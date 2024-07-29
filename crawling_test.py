8c464fbe8fd59ab072570f93b5726115d0dc8285
Seoa-S <0325sandy@ewhain.net>
2024-07-29 19:10:11 +0900

    update crawling_test.py: 전체 페이지 크롤링 성공

--- crawling_test.py
+++ crawling_test.py
@@ -1,17 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_term_links(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+def get_term_links(base_url, num_pages):
+    all_links = []
+    for page in range(1, num_pages + 1):
+        if page == 1:
+            url = base_url #첫 페이지 url
+        else:
+            url = f"{base_url}/page/{page}/"  # 두 번째 페이지부터의 URL 패턴
+        
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    #모든 카드의 링크 추출
-    cards = soup.find_all('div', class_='uc_post_title') #'uc_post_title' 클래스를 가진 모든 <div> 태그를 찾기
-    links = [card.find('a')['href'] for card in cards] # 각 <a> 태그에서 href 속성(링크)을 추출하여 리스트에 저장하기
+        # 모든 카드의 링크 추출
+        cards = soup.find_all('div', class_='uc_post_title')
+        links = [card.find('a')['href'] for card in cards]
 
-    return links
+        # 추출된 링크를 전체 링크 리스트에 추가
+        all_links.extend(links)
 
-url = 'https://uppity.co.kr/economy-dictionary/'
-term_links = get_term_links(url)  # 링크를 추출하는 함수 호출
+        # 디버깅: 각 페이지에서 추출된 링크 출력
+        #print(f"Extracted links from page {page}: {links}")
+    
+    return all_links
 
-print(term_links)
+base_url = 'https://uppity.co.kr/economy-dictionary'
+num_pages = 12  # 총 페이지 수
+term_links = get_term_links(base_url, num_pages)
+
+print("All extracted links:", term_links)
